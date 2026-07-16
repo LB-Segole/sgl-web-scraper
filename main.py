af99ee07edaab98ba633f1bee9f77dc648bf69ad
LB-Segole <segolelesego17@gmail.com>
2026-07-16 03:21:49 +0200

    reading and find sites title

--- main.py
+++ main.py
@@ -5,9 +5,19 @@
 # Looking at the requests document, i can gather how you fetch the dat you want
 
 import requests
+from bs4 import BeautifulSoup
+import json
+import time
 
-url = 'https://businesstech.co.za/news/'
-r = requests.get(url)
-r.text
+url = 'https://businesstech.co.za/news/5-things/862842/google-to-pay-elon-musk-r15-billion-every-month-and-top-south-african-retailer-closing-400-stores/' # the url that will be scraped
+page = requests.get(url) # requests the information from the url, sends back either 200, 403 - check website accesibility
+page.text # the entire HTM of the page as a string
 
-print(r.text)
+# now that we've fetched the site and know it's 200 and have seen the html page, we can turn to bs4
+# per documents:
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+title = soup.find('h1')
+
+print(title)
